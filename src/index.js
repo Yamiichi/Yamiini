c2ec917e4d4eeda7778d31653e911b88c1b8efe2
Dylan <dylan.languet@ensemblescolaire-niort.com>
2022-01-15 03:59:16 +0100

    push ta mere la

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-const { TOKEN } = require('./util/config');
 const YamiiniClient = require('./structures/YamiiniClient');
-
-let client = new YamiiniClient({
-  prefix: 'Y$'
-});
-
-client.login(TOKEN);
+let client = new YamiiniClient({ prefix: 'Y$' });
+client.start();
--- src/util/config.js
+++ src/util/config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  TOKEN : 'Njk4Mzc5Nzk3MDIxMTk2Mzkw.XpE-3g.70actygxmBbhGhB5jQrdb2pNrGM',
-  prefix : 'Y$',
-  link_invite : 'https://discord.com/api/oauth2/authorize?client_id=698379797021196390&permissions=8&scope=bot%20applications.commands',
-  BOTVERSION : '1.0',
-  NOM : 'Ce que tu veux'
+  TOKEN: 'Njk4Mzc5Nzk3MDIxMTk2Mzkw.XpE-3g.70actygxmBbhGhB5jQrdb2pNrGM',
+  prefix: 'Y$',
+  link_invite: 'https://discord.com/api/oauth2/authorize?client_id=698379797021196390&permissions=8&scope=bot%20applications.commands',
+  BOTVERSION: '1.0',
+  NOM: 'Yamiini',
+  MONGOSTRING: 'mongodb+srv://Yamiichi:Y4M11N1_31@cluster0.v7maj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 }
