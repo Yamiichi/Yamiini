eeb091e3d091bd248af30231cdc12cd5899df42b
Dylan <dylan.languet@ensemblescolaire-niort.com>
2022-01-16 03:48:04 +0100

    command prefix end + music add

---
+++ src/commands/dev/shutdown.js
@@ -0,0 +1,22 @@
+const { Command } = require('discord-akairo');
+
+class ShutdownCommand extends Command {
+  constructor() {
+    super('shutdown', {
+      aliases: ['shutdown', 'sd'],
+      category: 'Dev',
+      description: {
+        content: 'Commande pour arreter le bot.',
+        usage: 'shutdown',
+        examples: ['shutdown', 'sd']
+      },
+      ownerOnly: true
+    });
+  }
+
+  exec(message) {
+    require('child_process').execSync('pm2 stop 0');
+  }
+}
+
+module.exports = ShutdownCommand;
